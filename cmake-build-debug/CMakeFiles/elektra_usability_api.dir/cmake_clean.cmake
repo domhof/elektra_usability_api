06d87719982584860a1c8b126649ae921b7cee2c
Dominik Hofer <me@dominikhofer.com>
2017-03-14 22:27:11 +0100

    init

---
+++ cmake-build-debug/CMakeFiles/elektra_usability_api.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/elektra_usability_api.dir/wc.c.o"
+  "elektra_usability_api.pdb"
+  "elektra_usability_api"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/elektra_usability_api.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
